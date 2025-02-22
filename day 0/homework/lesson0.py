baf22a08a408b289d1c034d668b16e148c48e399
USERiuri <iurikaimerlishvili06@gmail.com>
2025-02-22 23:47:02 +0400

    first commit

---
+++ day 0/homework/lesson0.py
@@ -0,0 +1,34 @@
+name = "iuri"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "iuri" არის ცვლადისთვის მნიშვნელობა
+
+surname = "imerlishvili"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ცვლადი ან სტრინგი: ობიექტი
+
+name = "iuri" #ეს არის  str (string) ტიპის ცვლადი
+age=15 #ეს არის int (integer) მთელი რიცხვი
+height= 173.6 #ეს არის float ტიპის ცვლადი ანუ ათწილადი
+#booLean (booL) ტიპის ცვლადი
+
+knows_programming = True # True or False
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარულად)
+isUgly = False # ჯავასკრიპტული cameLcase
+
+print(name + " " + surname)
+# სტირინგი არის ბრწყალებში მოქცეული სიმბოლოები
+
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(height))
+#print(type(surname))
+#print(type(knows_programming))
+
+print( name + " " + str(age))
+
+print(height)
+print("iuri imerlishvili var 15 wlis daaxloebit 175 simagleshi")
