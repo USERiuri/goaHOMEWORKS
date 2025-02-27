2694582263b5397dbf5b52cd8ba7e93f8627c690
USERiuri <iurikaimerlishvili06@gmail.com>
2025-02-27 12:28:47 +0400

    second commit

---
+++ day 02/homework/lesson.py
@@ -0,0 +1,18 @@
+x = int("12")
+print (x)
+x = input("iuri")
+print(x)
+a = 33
+b = 200
+
+if b > a:
+  print("b is greater than a")
+
+a = 100
+b = 100
+if b > a:
+  print("b is larger than a")
+elif a==b:
+  print("a and b are equal")
+else: a>b
+print("a is larger than b")
